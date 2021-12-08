5d6a4bfefb64d5afc2cb21c08a683c114c4b194c
sencadas <joaosencadas@ipvc.pt>
2021-12-08 07:59:10 +0000

    Update - exemplo de getItem ao AsyncStorage

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
+  const [name, setName] = useState([]);
+  try {
+    AsyncStorage.getItem('name').then(value => {
+      setName(value);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Bem vindo, {name}</Text>
     </View>
   );
 };
