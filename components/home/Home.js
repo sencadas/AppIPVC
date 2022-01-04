a78891fec2416f3f7b76d455b47c11bbef52ee9a
sencadas <joaosencadas@ipvc.pt>
2022-01-04 19:40:19 +0000

    Exemplo de notificações

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,9 +1,14 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import PushNotification from 'react-native-push-notification';
 
 const Home = () => {
   const [name, setName] = useState([]);
+  PushNotification.createChannel({
+    channelId: 'test-channel',
+    channelName: 'Test Channel',
+  });
 
   try {
     AsyncStorage.getItem('name').then(value => {
@@ -13,9 +18,18 @@ const Home = () => {
     console.log(e);
   }
 
+  const handelNotification = () => {
+    PushNotification.localNotification({
+      channelId: 'test-channel',
+      title: 'Teste',
+      message: 'sdsddsds',
+    });
+  };
+
   return (
     <View>
       <Text>Bem vindo, {name}</Text>
+      <Button title="Press me" onPress={() => handelNotification()} />
     </View>
   );
 };
