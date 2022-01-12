2c366c0d528931b2fa2d6501faa8bdf406099c63
sencadas <joaosencadas@ipvc.pt>
2022-01-12 15:16:30 +0000

    calling plancurr from redux

--- components/planosCurriculares/PlanosCurriculares.js
+++ components/planosCurriculares/PlanosCurriculares.js
@@ -2,86 +2,23 @@ import React, {useState, useEffect} from 'react';
 import {List, Searchbar} from 'react-native-paper';
 import ListaAnos from './ListaAnos';
 import Styles from './Styles';
-import {SafeAreaView, View, FlatList, ActivityIndicator} from 'react-native';
-import {getPlanoCurricular, address} from '../../config';
+import Loading from '../universalComponents/Loading.js';
+import {SafeAreaView, View, FlatList} from 'react-native';
+import {connect} from 'react-redux';
+import {getPlanosCurriculares} from '../../store/planosCurriculares/actions';
 
-const formatData = data => {
-  let dataOrganized = [];
-  //get ano
-  for (let i = 0; i < data.length; i++) {
-    if (
-      !dataOrganized.some(e => e.ano === data[i].Ano_Curricular) &&
-      data[i].Ano_Curricular !== undefined
-    ) {
-      dataOrganized.push({
-        ano: data[i].Ano_Curricular,
-        UCs: {
-          primeiroSemestre: [],
-          segundoSemestre: [],
-        },
-      });
-    }
-  }
-  //get UCs e epara por semestre
-  for (let j = 0; j < data.length; j++) {
-    for (let i = 0; i < dataOrganized.length; i++) {
-      if (dataOrganized[i].ano === data[j].Ano_Curricular) {
-        if (data[j].Semestre_Curricular === 'S1') {
-          dataOrganized[i].UCs.primeiroSemestre.push(data[j]);
-        }
-        if (data[j].Semestre_Curricular === 'S2') {
-          dataOrganized[i].UCs.segundoSemestre.push(data[j]);
-        }
-      }
-    }
-  }
-  //organizar a por ano
-  dataOrganized.sort(function (a, b) {
-    return +(a.ano > b.ano) || +(a.ano === b.ano) - 1;
-  });
-
-  return dataOrganized;
-};
-
-const PlanosCurriculares = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+const PlanosCurriculares = ({data, fetchData}) => {
   const [searchQuery, setSearchQuery] = useState('');
   const onChangeSearch = query => setSearchQuery(query);
-
-  const URL = address + getPlanoCurricular;
-
   useEffect(() => {
-    fetch(URL)
-      .then(response => response.json())
-      .then(json => {
-        setData(formatData(json.planCurr));
-      })
-      .catch(error => {
-        throw error;
-      })
-      .finally(() => setLoading(false));
-  }, []);
-
-  /* const pesquisar = ({item}) => {
-    // when no input, show all
-    if (data.searchPhrase === '') {
-      return <Item name={item.name} details={item.details} />;
-    }
-    // filter of the name
-    if (
-      item.name
-        .toUpperCase()
-        .includes(data.searchPhrase.toUpperCase().trim().replace(/\s/g, ''))
-    ) {
-      return <Item name={item.name} details={item.details} />;
-    }
-  }; */
+    fetchData();
+  }, [fetchData]);
+  console.log(data);
 
   return (
     <SafeAreaView>
-      {isLoading ? (
-        <ActivityIndicator size={'large'} color={'#2fbbf0'} />
+      {data.loading ? (
+        <Loading />
       ) : (
         <View style={Styles.container}>
           <View style={Styles.textInputView}>
@@ -93,7 +30,7 @@ const PlanosCurriculares = () => {
           </View>
           <List.AccordionGroup>
             <FlatList
-              data={data}
+              data={data.data}
               keyExtractor={(item, index) => {
                 return index.toString();
               }}
@@ -106,4 +43,16 @@ const PlanosCurriculares = () => {
   );
 };
 
-export default PlanosCurriculares;
+const mapStateToProps = state => {
+  return {
+    data: state.PlanosCurricularesReducers,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchData: () => dispatch(getPlanosCurriculares()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlanosCurriculares);
